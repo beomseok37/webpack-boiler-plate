b6d610856c4485e996c6268c7b5bf3116b8fe1e2
hbs <bmtoss@naver.com>
2021-09-07 14:01:12 +0900

    동작은 하는 상태임...
    -html은 불러와지나, javascript가 생겨나진 않는다.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const indexRouter = require('./routes/index');
-
-
 const app = express();
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
 const port = process.env.port || 3000
 
 app.use(cookieParser());
@@ -13,15 +15,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.set('views',__dirname+"/views");
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname+'/src'));
 
-app.use(express.static(__dirname+'/public'));
+// app.use('/',indexRouter);
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats:{colors:true}
+  })
+);
 
-app.use('/',indexRouter);
-
-
-
+app.get("/", function(req, res) {});
 
 const server = app.listen(port);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: 
+  [
+    path.resolve(__dirname,'src','javascripts','index.js'),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template:'./src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+  ],
+  devtool: 'inline-source-map',
+}
